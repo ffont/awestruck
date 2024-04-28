accc69968bd41073d50d5e3e10bad1823501c18b
ffont <frederic.font@gmail.com>
2024-04-28 12:59:25 +0200

    Working sonic-pi based version

--- controller/server.py
+++ controller/server.py
@@ -1,10 +1,10 @@
-import sys
+import sys, os
 from flask import Flask, render_template, request
-from pythonosc import udp_client
 import psonic
-#from algoritme import sonic_pi_algo
+from algoritme import sonic_pi_algo
+import random
 
-sonic_pi_algo = """
+sonic_pi_algo_test = """
 # Fm Noise
 
 # Coded by Sam Aaron
@@ -19,44 +19,28 @@ end
 
 """
 
-osc_client = None
 psonic.set_server_parameter('127.0.0.1', 4557, 4559)
 
 app = Flask(__name__)
-
-
-def send_osc_message(address, value):
-    global osc_client
-    if osc_client is None:
-        try:
-            osc_client = udp_client.SimpleUDPClient('worker', 57120)
-        except Exception:
-            # Could not establish connection
-            log("Could not establish osc connection")
-            return False
-    log(f"Sending osc message! {address} {value}")
-    osc_client.send_message(f'/{address}', float(value))
-    return True
     
 def log(message):
     print(message)
     sys.stdout.flush()
 
-@app.route('/send_osc')
-def send_osc():
-    # Endpoint to receive OSC message command via HTTP
-    # Will forward the messages to the worker service
-    address = request.args.get('address')
-    value = request.args.get('value')
-    result = send_osc_message(address, value)
-    return {'result': result}
-
 
 @app.route('/start')
 def start():
-    new_seed = request.args.get('seed', default=1234, type=int)
+    # Make jack connections from SC to webrtc server (in case they did not exist)
+    os.system('jack_connect SuperCollider:out_1 webrtc-server:in_jackaudiosrc0_1')
+    os.system('jack_connect SuperCollider:out_2 webrtc-server:in_jackaudiosrc0_2')
+    os.system('jack_lsp -c')
+
+    # Select new seed and run algorithm
+    new_seed = request.args.get('seed', default=-1, type=int)
+    if new_seed == -1:
+        new_seed = random.randint(0, 999999)
     log(f'\n**********Stopping and setting new seed: {new_seed}************\n')
     psonic.stop()
-    psonic.run(sonic_pi_algo)
+    psonic.run(sonic_pi_algo.format(new_seed))
     return {'result': 'started'}
-    #psonic.run(sonic_pi_algo.format(new_seed))
+    
--- worker_startup.sh
+++ worker_startup.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 (nc -lk -u 127.0.0.1 -p 4558 &) && 
-(jackd -r --port-max 20 -d dummy &) && sleep 3 && 
-jack_lsp -c && 
-(/usr/lib/sonic-pi/server/bin/sonic-pi-server.rb &) && sleep 1 &&
-(/app/webrtc-server &) &&
-(flask --app server run --host=0.0.0.0 --port 54321)
+(jackd -r -d dummy &) && sleep 1 &&  # --port-max 20 <- this seems to make things fail!
+(/usr/lib/sonic-pi/app/server/ruby/bin/sonic-pi-server.rb &) &&  # start sonic pi, but don't load any patch
+(/app/webrtc-server &) &&  # start go server that handles the gstreamer->webrtc stuff
+(flask --app server run --host=0.0.0.0 --port 54321)  # start the flask server that controls the things!
